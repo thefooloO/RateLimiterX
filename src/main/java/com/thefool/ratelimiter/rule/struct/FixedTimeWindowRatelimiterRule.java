f9f0403faef393dff56bffaedcc572aac5108394
thefooloO <1031458146@qq.com>
2022-06-17 16:11:49 +0800

    code

--- src/main/java/com/thefool/ratelimiter/rule/struct/FixedTimeWindowRatelimiterRule.java
+++ src/main/java/com/thefool/ratelimiter/rule/struct/FixedTimeWindowRatelimiterRule.java
@@ -1,8 +1,11 @@
 package com.thefool.ratelimiter.rule.struct;
 
+import lombok.Data;
+
 /**
  * 固定时间窗口限流规则
  */
+@Data
 public class FixedTimeWindowRatelimiterRule {
     int limit;
     int unit = 1;
